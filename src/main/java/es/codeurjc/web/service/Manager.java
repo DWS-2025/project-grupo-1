d8079c058a93a95ee3b97e7f1cbcecd1b4f6089c
Julioolpz <juliolopezdelucas2704@gmail.com>
2025-02-27 16:26:39 +0100

    Avances en el manager, cambio de donde se crean las secciones

--- src/main/java/es/codeurjc/web/service/Manager.java
+++ src/main/java/es/codeurjc/web/service/Manager.java
@@ -1,4 +1,4 @@
-/*package es.codeurjc.web.service;
+package es.codeurjc.web.service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,27 +15,34 @@ import es.codeurjc.web.Model.Post;
 import es.codeurjc.web.Model.Section;
 import es.codeurjc.web.Model.User;
 import jakarta.annotation.PostConstruct;
-
+import es.codeurjc.web.Repository.UserRepository;
+import es.codeurjc.web.Repository.CommentRepository;
+import es.codeurjc.web.Repository.SectionRepository;
 
 @Component
 public class Manager {
     @Autowired
-    private UserService userRepository;
+    private UserRepository userRepository;
     @Autowired
     private PostService postRepository;
     @Autowired
-    private SectionService sectionRepository;
+    private SectionRepository sectionRepository;
     @Autowired
-    private CommentService commentRepository;
+    private CommentRepository commentRepository;
 
     @PostConstruct
 	public void init() {
 
 		// Default user
-		User michel = new User("mainUser", "ContraseñaSegura", "mainUser@gmail.com");
-
+		User mainUser = new User("mainUser", "ContraseñaSegura", "mainUser@gmail.com");
 		// Other users
-		User carlos = new User();
+        User user1 = new User("user1", "Contraseña1", "user1@gmail.com");
+        User user2 = new User("user2", "Contraseña2", "user2@gmail.com");
+        User user3  = new User("user3", "Contraseña3", "user3@gmail.com");
+        User user4 = new User("user4", "Contraseña4", "user4@gmail.com");
+        User user5 = new User("user5", "Contraseña5", "user5@gmail.com");
+		User user6 = new User("user6", "Contraseña5", "user6r@gmail.com");
+
 
 		// Some examples of posts and comments
 		Post post = new Post();
@@ -52,8 +59,24 @@ public class Manager {
 		post.getComments().add();
 		commentRepository.save();
 
-		postRepository.save();
-		userRepository.save();
-		userRepository.save();
+		//Create new sections
+        Section defaultSection1 = new Section("Reversing","Análisis y descompilación de binarios para entender su funcionamiento.", "reversing.png");
+        Section defaultSection2 = new Section("Hacking Web", "Explotación de vulnerabilidades en aplicaciones web.","hacking_web.png");
+        Section defaultSection3 = new Section("Escalada de Privilegios","Métodos para obtener acceso administrativo en Windows.", "escalada_windows.jpeg");
+        Section defaultSection4 = new Section("Hardware Hacking","Explotación de vulnerabilidades a nivel de hardware.", "hardware.jpeg");
+        Section defaultSection5 = new Section("WiFi", "Ataques y auditorías de seguridad en redes inalámbricas.","wifi.jpg");
+
+		userRepository.save(mainUser);
+		userRepository.save(user1);
+		userRepository.save(user2);
+		userRepository.save(user3);
+		userRepository.save(user4);
+		userRepository.save(user5);
+		postRepository.save(user6);
+		sectionRepository.save(defaultSection1);
+		sectionRepository.save(defaultSection2);
+		sectionRepository.save(defaultSection3);
+		sectionRepository.save(defaultSection4);
+		sectionRepository.save(defaultSection5);
 	}
-} */
+} 
