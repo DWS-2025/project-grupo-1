60bd6656118194eb3025ef636df5be6edc23bbe9
markel-f <m.fernandezpi.2024@alumnos.urjc.es>
2025-03-28 12:41:26 +0100

    se ha creado un adviceController, comentado por que da errores

---
+++ src/main/java/es/codeurjc/web/restController/adviceController.java
@@ -0,0 +1,16 @@
+package es.codeurjc.web.restController;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+/* 
+@ControllerAdvice
+class NoSuchElementExceptionControllerAdvice {
+ @ResponseStatus(HttpStatus.NOT_FOUND)
+ @ExceptionHandler(NoSuchElementException.class)
+ public void handleNoTFound() {
+ }
+}
+ */
