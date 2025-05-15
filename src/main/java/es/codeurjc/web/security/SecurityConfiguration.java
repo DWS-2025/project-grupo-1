85d89d12464068ba006c6f49e99986ccedf83e0e
EvaFdezLpz <e.fernandezl.2023@alumnos.urjc.es>
2025-05-15 22:28:27 +0200

    añadido apiFilterChain, esta sin terminar. Es la idea principal

--- src/main/java/es/codeurjc/web/security/SecurityConfiguration.java
+++ src/main/java/es/codeurjc/web/security/SecurityConfiguration.java
@@ -3,16 +3,20 @@ package es.codeurjc.web.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import es.codeurjc.web.service.RepositoryUserDetailsService;
 
-@Configuration 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 	@Autowired
@@ -33,34 +37,66 @@ public class SecurityConfiguration {
 		return authProvider;
 	}
 
+	@Bean
+	@Order(1)
+	public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+		http.authenticationProvider(authenticationProvider());
+
+		http.authorizeHttpRequests(authorize -> authorize
+				// PRIVATE ENDPOINTS
+				.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("USER", "ADMIN")
+				.requestMatchers(HttpMethod.POST, "/api/**").hasRole("USER")
+				.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("USER")
+				.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
+
+				// PUBLIC ENDPOINTS
+				.anyRequest().permitAll());
+
+		// Disable Form login Authentication
+		http.formLogin(formLogin -> formLogin.disable());
+
+		// Disable CSRF protection (it is difficult to implement in REST APIs)
+		http.csrf(csrf -> csrf.disable());
+
+		// Disable Basic Authentication
+		http.httpBasic(httpBasic -> httpBasic.disable());
+
+		// Stateless session
+		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		// Add JWT Token filter SE DEBE IMPLEMENTAR
+		// http.addFilterBefore(jwtRequestFilter,
+		// UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+
+	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
+	@Order(2)
+	public SecurityFilterChain webFilterChain(HttpSecurity http) throws Exception {
+
 		http.authenticationProvider(authenticationProvider());
-		
+
 		http
-			.authorizeHttpRequests(authorize -> authorize
-					// PUBLIC PAGES, in /assets/** maybe we should just specify the files we need
-					.requestMatchers("/", "/assets/**", "/vendor/**").permitAll()
-					// PRIVATE PAGES
-					.anyRequest().authenticated())
-			.formLogin(formLogin -> formLogin
-					.loginPage("/login")
-					.failureUrl("/loginerror")
-					.defaultSuccessUrl("/home")
-					.permitAll()
-			)
-			.logout(logout -> logout
-					.logoutUrl("/logout")
-					.logoutSuccessUrl("/login")
-					.permitAll()
-			);
-		
+				.authorizeHttpRequests(authorize -> authorize
+						// PUBLIC PAGES, in /assets/** maybe we should just specify the files we need
+						.requestMatchers("/", "/assets/**", "/vendor/**").permitAll()
+						// PRIVATE PAGES
+						.anyRequest().authenticated())
+				.formLogin(formLogin -> formLogin
+						.loginPage("/login")
+						.failureUrl("/loginerror")
+						.defaultSuccessUrl("/home")
+						.permitAll())
+				.logout(logout -> logout
+						.logoutUrl("/logout")
+						.logoutSuccessUrl("/login")
+						.permitAll());
+
 		// Disable CSRF at the moment
 		http.csrf(csrf -> csrf.disable());
 
 		return http.build();
 	}
-
 }
