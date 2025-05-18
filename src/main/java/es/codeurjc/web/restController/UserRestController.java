f9720b19f4be768cb58f63994f5999ef033159e8
markel-f <m.fernandezpi.2024@alumnos.urjc.es>
2025-05-18 11:30:38 +0200

    arreglo en following y en discover, no iban

--- src/main/java/es/codeurjc/web/restController/UserRestController.java
+++ src/main/java/es/codeurjc/web/restController/UserRestController.java
@@ -1,7 +1,5 @@
 package es.codeurjc.web.restController;
 
-import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
-
 import java.io.IOException;
 import java.net.URI;
 import java.sql.SQLException;
@@ -11,8 +9,8 @@ import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
 import es.codeurjc.web.dto.UserBasicDTO;
 import es.codeurjc.web.dto.UserDTO;
